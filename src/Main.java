353bb2e87cf0a09848094c595866397f32e690fd
Oleksandr Zhurba <starkyzh@gmail.com>
2023-05-01 23:27:00 +0200

    solve with keyboard input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
+    private static final String SEP = ": ";
+
+    public static void main(String[] args) throws IOException {
         /*
         Задача 1
         Программисты, как вы уже знаете, постоянно учатся, а в общении между собой используют весьма специфический язык.
@@ -19,6 +26,23 @@ public class Main {
         Если слова в словаре нет, программа должна вывести "Не найдено", без кавычек.
          */
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Pls input count of record:");
+        int m = Integer.parseInt(br.readLine());
+        HashMap<String, String> dict = new HashMap<>();
+        String[] lineArray;
+        for (int i = 0; i < m; i++) {
+            System.out.print("Pls input one record with separator as '" + SEP + "':");
+            String line = br.readLine();
+            lineArray = line.split(SEP);
+            dict.put(lineArray[0].toLowerCase(), lineArray[1]);
+        }
+        System.out.println(dict);
+
+        System.out.print("Input search word:");
+        String search = br.readLine();
+        String searchAsKey = search.toLowerCase();
+        System.out.println(dict.getOrDefault(searchAsKey, "Не найдено\n"));
 
     }
 }
